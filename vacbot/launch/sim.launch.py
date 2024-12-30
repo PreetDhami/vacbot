75d2822efbfe08938f89dffa3496f8f58945cbe1
Preet Dhami <preetjot02@gmail.com>
2024-12-30 13:41:03 -0800

    added nav2 integration

--- vacbot/launch/sim.launch.py
+++ vacbot/launch/sim.launch.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 from os.path import join
 
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, RegisterEventHandler, ExecuteProcess, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -29,10 +31,13 @@ def generate_launch_description():
     pkg_share = FindPackageShare(package='vacbot').find('vacbot')
 
     # Get URDF via xacro
-    robot_description = {'robot_description': Command([PathJoinSubstitution([FindExecutable(name="xacro")]), " ", pkg_share, '/urdf/vacbot.urdf.xacro'])}
+    robot_description = {'robot_description': Command(["xacro ", pkg_share, '/urdf/vacbot.urdf.xacro'])}
     robot_controllers = PathJoinSubstitution([pkg_share,'config','ros2_controllers.yaml'])
     rviz_config_file = PathJoinSubstitution([pkg_share, 'config', 'vacbot.rviz'])
     world_path = PathJoinSubstitution([pkg_share, 'world', 'my_world.sdf'])
+    nav2_params_file = PathJoinSubstitution([pkg_share, 'config', 'nav2_params'])
+
+
 
     world = LaunchConfiguration('world')
     world_arg = DeclareLaunchArgument(
@@ -41,13 +46,12 @@ def generate_launch_description():
         description='World to load'
     )
 
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+    sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value=use_sim_time,
+        description='If true, use simulated clock')
 
-    control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[robot_controllers],
-        output="both",
-    )
     
     robot_state_pub_node = Node(
         package="robot_state_publisher",
@@ -65,15 +69,15 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["diff_cont", "--param-file", robot_controllers],
+        arguments=["diff_cont", '--param-file', robot_controllers]
     )
 
     # Delay start of joint_state_broadcaster after `robot_controller`
     # TODO(anyone): This is a workaround for flaky tests. Remove when fixed.
     delay_joint_state_broadcaster_after_robot_controller_spawner = RegisterEventHandler(
         event_handler=OnProcessExit(
-            target_action=robot_controller_spawner,
-            on_exit=[joint_state_broadcaster_spawner],
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_controller_spawner],
         )
     )
 
@@ -85,40 +89,38 @@ def generate_launch_description():
         arguments=["-d", rviz_config_file],
     )
 
-    # gazebo = IncludeLaunchDescription(PythonLaunchDescriptionSource([
-    #     PathJoinSubstitution([FindPackageShare(package='ros_gz_sim'),'launch', 'gz_sim.launch.py'])]),
-    #     launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items())
+    gazebo = IncludeLaunchDescription(PythonLaunchDescriptionSource([join(
+        get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+        launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+    )
 
+    gz_spawn_entity = Node(
+    	package='ros_gz_sim', 
+    	executable='create',
+        arguments=['-name', 'vacbot', '-topic', 'robot_description', '-z', '0.1'],
+        output='screen'
+    )
 
-    # bridge_params = join(pkg_share,'config','gz_bridge.yaml')
-    # ros_gz_bridge = Node(
-    #     package="ros_gz_bridge",
-    #     executable="parameter_bridge",
-    #     arguments=[
-    #         '--ros-args',
-    #         '-p',
-    #         f'config_file:={bridge_params}',
-    #     ]
-    # )
-
-
-    # create = Node(
-    # 	package='ros_gz_sim', 
-    # 	executable='create',
-    #     arguments=['-name', 'vacbot', '-topic', 'robot_description', '-z', '0.1'],
-    #     output='screen'
-    # )
-
+    bridge_params = join(get_package_share_directory("vacbot"),'config','gz_bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
+    )
 
 
     return LaunchDescription([
-        control_node,
         robot_state_pub_node,
         robot_controller_spawner,
         delay_joint_state_broadcaster_after_robot_controller_spawner,
         rviz_node,
-        # world_arg,
-        # gazebo,
-        # ros_gz_bridge,
-        # create
+        world_arg,
+        gazebo,
+        ros_gz_bridge,
+        gz_spawn_entity,
+        sim_time_arg,
     ])
